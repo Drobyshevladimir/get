fc4068cc03f315079182d923a634f9060264f9db
Владимир Дробышев <drobyshev.va@phystech.edu>
2024-03-18 12:02:59 +0300

    4-3

--- 4/4-3-pwm.py
+++ 4/4-3-pwm.py
@@ -1,20 +1,21 @@
 import RPi.GPIO as GPIO
-
+from time import sleep
 def decimal2binary(chislo):
     return [int(element) for element in bin(chislo)[2:].zfill(8)]
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(24, GPIO.OUT)
-p = GPIO.PWM(24,100)
-p.start(1)
+GPIO.setup(21, GPIO.OUT)
+p = GPIO.PWM(21,  100)
+p.start(0)
+try:
+    while True:
+        t=int(input())
+        p.ChangeDutyCycle(t)
+        print((1000*3.3*t/100)//1/1000)
+        sleep(0.5)
+except ValueError:
+    print('wrong value')
 p.stop()
-input()
-p.stop()
+GPIO.output(21,0)
 GPIO.cleanup()
-#while True:
-#    a=float(input())
- #   p = GPIO.PWM(24,a)
-
-
-
